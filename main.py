f26f9645013d571342b46255def184c0d00c7dfb
kwonsj0825 <kwonsj0825@naver.com>
2025-03-14 09:32:48 +0900

    feat: main.py dev branch에서 수정

---
+++ main.py
@@ -0,0 +1,9 @@
+
+iser@DESKTOP-0J48QFV MINGW64 ~/Desktop/git실습 (master)
+$ ls -a
+./  ../  .git/  main.py
+
+User@DESKTOP-0J48QFV MINGW64 ~/Desktop/git실습 (master)
+$ git branch
+print("Gat의 세계에 오신 것을 환영합니다")
+print("이건 dev branch에서 작성한 내용입니다.")
